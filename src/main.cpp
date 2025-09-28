dd879a7d612a63ab41102d156bbdb66c7fc62e91
rapeeploy <66050353@kmitl.ac.th>
2025-09-29 06:37:50 +0700

    lab-8_3

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,14 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 
 Window mainWindow;
-std::vector<Mesh*> meshList;
+std::vector<Mesh *> meshList;
 std::vector<Shader> shaderList;
 
 // Vertex Shader
-static const char* vShader = "Shaders/shader.vert";
+static const char *vShader = "Shaders/shader.vert";
 
 // Fragment Shader
-static const char* fShader = "Shaders/shader.frag";
+static const char *fShader = "Shaders/shader.frag";
 
 void CreateShaders()
 {
@@ -48,10 +48,18 @@ void CreateTriangle()
 
     unsigned int indices[] =
         {
-            0, 3, 1,
-            1, 3, 2,
-            2, 3, 0,
-            0, 1, 2,
+            0,
+            3,
+            1,
+            1,
+            3,
+            2,
+            2,
+            3,
+            0,
+            0,
+            1,
+            2,
         };
 
     Mesh *obj1 = new Mesh();
@@ -72,9 +80,9 @@ int main()
 
     GLuint uniformModel = 0, uniformProjection = 0, uniformView = 0;
 
-    // glm::mat4 projection = glm::perspective(45.0f, (GLfloat) mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(), 0.1f, 100.0f);
-    glm::mat4 projection = glm::ortho(-4.0f, 4.0f, -3.0f, 3.0f, 0.1f, 100.0f);
-    
+    glm::mat4 projection = glm::perspective(45.0f, (GLfloat) mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(), 0.1f, 100.0f);
+    // glm::mat4 projection = glm::ortho(-4.0f, 4.0f, -3.0f, 3.0f, 0.1f, 100.0f);
+
     while (!mainWindow.getShouldClose())
     {
         // Get + Handle user input events
@@ -87,8 +95,30 @@ int main()
         // draw here
         shaderList[0].UseShader();
         uniformModel = shaderList[0].GetUniformLocation("model");
+        uniformView = shaderList[0].GetUniformLocation("view");
         uniformProjection = shaderList[0].GetUniformLocation("projection");
-        
+
+        glm::mat4 view(1.0f);
+
+        // glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 10.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f, -0.3f, -1.0f);
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        glm::vec3 cameraDirection = glm::normalize(cameraTarget - cameraPos);
+        glm::vec3 cameraRight = glm::normalize(glm::cross(cameraDirection, up));
+        glm::vec3 cameraUp = glm::normalize(glm::cross(cameraRight, cameraDirection));
+
+        glm::mat4 cameraPosMat(1.0f);
+        cameraPosMat[3][0] = -cameraPos.x;
+        cameraPosMat[3][1] = -cameraPos.y;
+        cameraPosMat[3][2] = -cameraPos.z;
+        glm::mat4 cameraRotateMat(1.0f);
+        cameraRotateMat[0] = glm::vec4(cameraRight.x, cameraUp.x, -cameraDirection.x, 0.0f);
+        cameraRotateMat[1] = glm::vec4(cameraRight.y, cameraUp.y, -cameraDirection.y, 0.0f);
+        cameraRotateMat[2] = glm::vec4(cameraRight.z, cameraUp.z, -cameraDirection.z, 0.0f);
+        view = cameraRotateMat * cameraPosMat;
+
         glm::vec3 pyramidPosition[] = {
             glm::vec3(0.0f, 0.0f, -2.5f),
             glm::vec3(2.0f, 5.0f, -15.0f),
@@ -99,18 +129,19 @@ int main()
             glm::vec3(1.3f, -2.0f, -2.5f),
             glm::vec3(1.5f, 2.0f, -2.5f),
             glm::vec3(1.5f, 0.2f, -1.5f),
-            glm::vec3(-1.3f, 1.0f, -1.5f)
-        };
+            glm::vec3(-1.3f, 1.0f, -1.5f)};
 
         // Object
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++)
+        {
             glm::mat4 model(1.0f);
 
             model = glm::translate(model, pyramidPosition[i]);
-            model = glm::rotate(model, glm::radians(2.0f*i),glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::rotate(model, glm::radians(2.0f * i), glm::vec3(1.0f, 0.3f, 0.5f));
             model = glm::scale(model, glm::vec3(0.8f, 0.8f, 0.8f));
 
             glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+            glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(view));
             glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
             meshList[i]->RenderMesh();
