62f1c8cc766836520807996cb2f18a2932cc83a2
rapeeploy <66050353@kmitl.ac.th>
2025-09-29 06:51:09 +0700

    lab-8_final

--- src/main.cpp
+++ src/main.cpp
@@ -100,8 +100,7 @@ int main()
 
         glm::mat4 view(1.0f);
 
-        // glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
-        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 10.0f);
+        glm::vec3 cameraPos = glm::vec3(1.0f, 0.5f, 2.0f);
         glm::vec3 cameraTarget = glm::vec3(0.0f, -0.3f, -1.0f);
         glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 
